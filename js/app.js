e22b69b7ba437f452b86db71a75d8864b684a314
carlosmora97 <carlosmora1997@gmail.com>
2024-03-14 10:22:37 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-
+var url = window.location.href;
+var swLocation = '/pwa-example/sw.js'
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js')
+
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation)
 }
 
 
